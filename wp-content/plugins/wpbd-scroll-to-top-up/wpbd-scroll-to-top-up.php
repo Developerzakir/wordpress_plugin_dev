ae4db90570dc2d248df3be49d27a22fbd414b40b
zakir Hossen <zakirhossen082@gmail.com>
2025-04-28 13:27:51 +0600

    Plugin Info Setup

--- wp-content/plugins/wpbd-scroll-to-top-up/wpbd-scroll-to-top-up.php
+++ wp-content/plugins/wpbd-scroll-to-top-up/wpbd-scroll-to-top-up.php
@@ -1,5 +1,18 @@
 <?php 
 
-
+/**
+ * Plugin Name:       Wpbd Scroll To Top Up
+ * Plugin URI:        https://wordpress.org/plugins/wpbd-scroll-to-top-up/
+ * Description:       Wpbd Simple Scroll to top plugin will help you to enable Back to Top button to your WordPress website.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.1
+ * Author:            Zakir Hossain
+ * Author URI:        https://developerzakir.blogspot.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/Developerzakir
+ * Text Domain:       wpbdstt
+ */
 
 ?>
