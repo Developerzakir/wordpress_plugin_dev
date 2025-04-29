5c08688b749daffa45da5e38c3ba2d358fc1f433
zakir Hossen <zakirhossen082@gmail.com>
2025-04-29 11:28:04 +0600

    Admin login page static design added (Static Plugin Creation)

--- wp-content/plugins/customizer-login-page-wp/customizer-login-page-wp.php
+++ wp-content/plugins/customizer-login-page-wp/customizer-login-page-wp.php
@@ -22,5 +22,41 @@ function wpclp_add_theme_page(){
 add_action( 'admin_menu', 'wpclp_add_theme_page' );
 
 
+  /**
+   * Plugin Callback
+   */
+  function wpclp_create_page(){
+    echo 'Plugin Option';
+  }
+
+
+// Loading CSS file
+function wpclp_login_enqueue_register(){
+  wp_enqueue_style( 'wpclp_login_enqueue', plugins_url( 'css/wpclp-styles.css', __FILE__ ), false, "1.0.0");
+
+}
+add_action('login_enqueue_scripts', 'wpclp_login_enqueue_register');
+
+// Changing Login form logo
+function wpclp_login_logo_change(){
+  ?>
+  <style>
+    #login h1 a, .login h1 a{
+      background-image: url(<?php print plugin_dir_url( __FILE__ ) . '/img/logo-sm.png'; ?>);
+    }
+  </style>
+
+  <?php
+}
+add_action( 'login_enqueue_scripts', 'wpclp_login_logo_change');
+
+// Changing Login form logo url
+function wpclp_login_logo_url_change(){
+  return home_url();
+}
+add_filter( 'login_headerurl', 'wpclp_login_logo_url_change');
+
+
+
 
  ?>
