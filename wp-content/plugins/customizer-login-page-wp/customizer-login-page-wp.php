de214124b8b481c6a2dc600f1f3ddc61b762435d
zakir Hossen <zakirhossen082@gmail.com>
2025-04-29 13:08:20 +0600

    plugin options page design

--- wp-content/plugins/customizer-login-page-wp/customizer-login-page-wp.php
+++ wp-content/plugins/customizer-login-page-wp/customizer-login-page-wp.php
@@ -22,6 +22,16 @@ function wpclp_add_theme_page(){
 add_action( 'admin_menu', 'wpclp_add_theme_page' );
 
 
+  /*
+  * Plugin Option Page Style
+  */
+  function wpclp_add_theme_css(){
+    wp_enqueue_style( 'wpclp-admin-style', plugins_url( 'css/wpclp-admin-style.css', __FILE__ ), false, "1.0.0");
+  
+  }
+  add_action('admin_enqueue_scripts', 'wpclp_add_theme_css');
+
+
   /**
    * Plugin Callback
    */
@@ -32,16 +42,38 @@ add_action( 'admin_menu', 'wpclp_add_theme_page' );
         <h3 id="title"><?php print esc_attr( 'Login Page Customizer' ); ?></h3>
         <form action="options.php" method="post">
           <?php wp_nonce_field('update-options'); ?>
-          <label for="wpclp-primary-color" name="wpclp-primary-color"><?php print esc_attr( 'Primary Color' ); ?></label>
-          <input type="color" name="wpclp-primary-color" value="<?php print get_option('wpclp-primary-color') ?>">
+
+            <!-- Primary Color -->
+            <label for="wpclp-primary-color" name="wpclp-primary-color"><?php print esc_attr( 'Primary Color' ); ?></label>
+            <input type="color" name="wpclp-primary-color" value="<?php print get_option('wpclp-primary-color') ?>">
+
+            <!-- Secondary Color -->
+            <label for="wpclp-sec-color" name="wpclp-sec-color"><?php print esc_attr( 'Secondary Color' ); ?></label>
+            <input type="color" name="wpclp-sec-color" value="<?php print get_option('wpclp-sec-color') ?>">
+
+            <!-- Main Logo -->
+            <label for="wpclp-logo-image-url" name="wpclp-logo-image-url"><?php print esc_attr( 'Upload your logo' ); ?></label>
+            <input type="text" name="wpclp-logo-image-url" value="<?php print get_option('wpclp-logo-image-url') ?>" placeholder="Paste your Logo URL here">
+
+            <!-- Background Image -->
+            <label for="wpclp-custom-bg-image" name="wpclp-custom-bg-image"><?php print esc_attr( 'Upload your Bacground Image' ); ?></label>
+            <input type="text" name="wpclp-custom-bg-image" value="<?php print get_option('wpclp-custom-bg-image') ?>" placeholder="Paste your Background Image URL here">
+
+            <!-- Bacground Brightness -->
+            <label for="wpclp-custom-bg-brightness" name="wpclp-custom-bg-brightness"><?php print esc_attr( 'Background Brightness {Between 0.1 to 0.9}' ); ?></label>
+            <input type="text" name="wpclp-custom-bg-brightness" value="<?php print get_option('wpclp-custom-bg-brightness') ?>" placeholder="Between 0.1 to 0.9">
 
 
           <input type="hidden" name="action" value="update">
-          <input type="hidden" name="page_options" value="wpclp-primary-color">
+          <input type="hidden" name="page_options" value="wpclp-primary-color, wpclp-logo-image-url, wpclp-custom-bg-image, wpclp-sec-color, wpclp-custom-bg-brightness">
           <input type="submit" name="submit" class="button button-primary" value="<?php _e('Save Changes', 'wpclp') ?>">
         </form>
       </div>
-      <div class="wpclp_sidebar_area"></div>
+
+      <div class="wpclp_sidebar_area wpclp_common">
+      <h3 id="title"><?php print esc_attr( 'About Author' ); ?></h3>
+      <p>I'm <strong><a href=" https://developerzakir.blogspot.com/" target="_blank">Zakir Hossen</a></strong> a Front End Web developer who is passionate about making error-free websites with 100% client satisfaction. I have a passion for learning and sharing my knowledge with others as publicly as possible. I love to solve real-world problems.</p>
+      </div>
     </div>
   <?php
   }
@@ -58,9 +90,33 @@ add_action('login_enqueue_scripts', 'wpclp_login_enqueue_register');
 function wpclp_login_logo_change(){
   ?>
   <style>
-    #login h1 a, .login h1 a{
-      background-image: url(<?php print plugin_dir_url( __FILE__ ) . '/img/logo-sm.png'; ?>);
-    }
+        #login h1 a, .login h1 a{
+        background-image: url(<?php print get_option('wpclp-logo-image-url'); ?>) !important;
+        }
+        #login form p.submit input {
+        background: <?php print get_option('wpclp-logo-image-url'); ?>;
+        }
+        .login #login_error,
+        .login .message,
+        .login .success {
+        border-left: 4px solid <?php print get_option('wpclp-primary-color'); ?> !important;
+        }
+        input#user_login,
+        input#user_pass {
+        border-left: 4px solid <?php print get_option('wpclp-primary-color'); ?> !important;
+        }
+        #login form p.submit input {
+        background: <?php print get_option('wpclp-primary-color'); ?> !important;
+        }
+        .login #backtoblog a {
+        background: <?php print get_option('wpclp-sec-color'); ?> !important;
+        }
+        body.login {
+        background-image: url(<?php print get_option('wpclp-custom-bg-image'); ?>) !important;
+        }
+        body.login::after {
+        opacity: <?php print get_option('wpclp-custom-bg-brightness'); ?> !important;
+        }
   </style>
 
   <?php
