8e92897eb92ee9bbbb1a648c7685b8692f758918
zakir Hossen <zakirhossen082@gmail.com>
2025-04-29 10:50:47 +0600

    Plugin Page Creation

--- wp-content/plugins/customizer-login-page-wp/customizer-login-page-wp.php
+++ wp-content/plugins/customizer-login-page-wp/customizer-login-page-wp.php
@@ -2,8 +2,8 @@
 /**
  * Plugin Name:       Wordpress Customizer Login Page
  * Plugin URI:        https://wordpress.org/plugins/wp-customizer-login-page-wp/
- * Description:       The Customizer Login Page plugin will help you to enable a custom login page to your WordPress website.
- * Version:           1.0.0
+ * Description:       The WP Customizer Login Page plugin will help you to enable a custom login page to your WordPress website.
+ * Version:           1.1.1
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Zakir Hossen
@@ -12,6 +12,14 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       wpclp
  */
+
+  /*
+ * Plugin Option Page Function
+ */
+function wpclp_add_theme_page(){
+    add_menu_page( 'Login Option for Admin', 'Login Option', 'manage_options', 'wpclp-plugin-option', 'wpclp_create_page', 'dashicons-unlock', 101 );
+}
+add_action( 'admin_menu', 'wpclp_add_theme_page' );
 
 
 
